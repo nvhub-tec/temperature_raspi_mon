7d8bcf4ee074f9c0092aa6b84a9950a43873bb6a
nvhub-tec <noah.vernon@vreoinnovation.com>
2025-01-15 12:26:13 +0000

    changed slashes

--- raspberry_temp/main.py
+++ raspberry_temp/main.py
@@ -1,6 +1,6 @@
 
 def read_temp_file():
-    with open(r"\sys\class\thermal\thermal_zone0\temp", "r") as file:
+    with open(r"//sys//class//thermal//thermal_zone0//temp", "r") as file:
                     temp_str = file.read() 
                     return int(temp_str) / 1000.0
                 
