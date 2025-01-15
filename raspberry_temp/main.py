4e38dcdf5239531243570b87ea248687f05de23c
nvhub-tec <noah.vernon@vreoinnovation.com>
2025-01-15 11:59:58 +0000

    commiting

---
+++ raspberry_temp/main.py
@@ -0,0 +1,9 @@
+
+def read_temp_file():
+    with open(r"\sys\class\thermal\thermal_zone0\temp", "r") as file:
+                    temp_str = file.read() 
+                    return int(temp_str) / 1000.0
+                
+cpu_temp = read_temp_file
+print(f"cpu temperature, degrees Celcius: ", {cpu_temp})
+            
