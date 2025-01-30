93e0b28cb4bc62a62bbc2f2786872a9b9fda692d
vrgitcode <noah.vernon@vreoinnovation.com>
2025-01-30 14:11:17 +0000

    refactored class to set default values, gpu temp not working

--- raspberry_temp/temperature_class.py
+++ raspberry_temp/temperature_class.py
@@ -1,38 +1,50 @@
 import subprocess
 
 class device_temperature:
-    """gpu and cpu temperature sampling methods with error handling"""
+    """
+    gpu and cpu temperature sampling methods with error handling
+    """
     
-    def __init__ (self, cpu_temperature, gpu_temperature):
-        self.cpu_temperature = cpu_temperature  #"//sys//class//thermal//thermal_zone0//temp"
-        self.gpu_temperature = gpu_temperature  #"vcgencmd measure_temp" #terminal command
+    def __init__ (
+        self,
+        cpu_thermal_zone_path: str = "//sys//class//thermal//thermal_zone0//temp", 
+        gpu_thermal_zone_path: str = "usr//bin//vcgencmd measure_temp"
+    ):
+        self.cpu_thermal_zone_path = cpu_thermal_zone_path
+        self.gpu_thermal_zone_path = gpu_thermal_zone_path
 
     
-    def read_gpu_temperature(self): #shell command, convert from byte str, format, handle errors
-        try:
-            output = subprocess.check_output(["//usr//bin//vcgencmd",
-                                              "measure_temp"]).decode("utf-8")
-            gpu_temp = output.split("=")[1].split("'")[0]
-            
-            return float(gpu_temp)
-        except FileNotFoundError:
-            raise Exception(
-                "Failed to read GPU temperature:'vcgencmd' command not found."
-            )
-        except ValueError:  
-            raise Exception(
-                "Failed to read GPU temperature: "
-                "Output from 'vcgencmd' is not a valid number."
-                            )
-        except subprocess.CalledProcessError as e:
-            raise Exception(
-                f"Failed to read GPU temperature:"
-                "'vcgencmd' command failed with error: {e}"
-                )
-        except Exception as e:
-            raise Exception(f"Failed to read GPU temperature: {e}")
+#    def read_gpu_temperature(self): 
+        """
+        shell command, convert from byte str, format, handle errors
+        - broken vcgencmd
+        """
+#        try:
+#            output = subprocess.check_output([self.gpu_thermal_zone_path]).decode("utf-8")
+#            gpu_temperature = output.split("=")[1].split("'")[0]
+#            
+#            return float(gpu_temperature)
+#        except FileNotFoundError:
+#            raise Exception(
+#                "Failed to read GPU temperature:'vcgencmd' command not found."
+#            )
+#        except ValueError:  
+#            raise Exception(
+#                "Failed to read GPU temperature: "
+#                "Output from 'vcgencmd' is not a valid number."
+#                            )
+#        except subprocess.CalledProcessError as e:
+#            raise Exception(
+#                f"Failed to read GPU temperature:"
+#                "'vcgencmd' command failed with error: {e}"
+#                )
+#        except Exception as e:
+#            raise Exception(f"Failed to read GPU temperature: {e}")
 
     def read_cpu_temperature(self):
-        with open("//sys//class//thermal//thermal_zone0//temp", "r") as file:
-            cpu_temp = file.read()
-            return int(cpu_temp) / 1000.0
+    """
+    opens the file containing current cpu temperature
+    """    
+        with open(self.cpu_thermal_zone_path, "r") as file:
+            cpu_temperature = file.read()
+            return int(cpu_temperature) / 1000.0
